e9f942c777304e466c2c9e2babe86c16adbd5284
Fernando <fernandohenrique.exe@gmail.com>
2024-05-30 02:44:45 +0000

    Ajuste Index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php include('templates/header.php'); ?>
 
-<h1>Products for Sale</h1>
-<a href="add_product.php">Add New Product</a>
-<?php include('list_products.php'); ?>
+<div class="container">
+    <div class="sidebar">
+        <!-- Seu menu aqui -->
+        <a href="index.php">Tela Inicial</a>
+        <a href="add_product.php">Cadastrar Itens</a>
+        <a href="view_items.php">Visualizar Itens</a>
+    </div>
+    <div class="content">
+        <h1>Products for Sale</h1>
+        <a href="add_product.php">Add New Product</a>
+        <?php include('list_products.php'); ?>
+    </div>
+</div>
 
 <?php include('templates/footer.php'); ?>
-
-<?php include('templates/header.php'); ?>
-
-
